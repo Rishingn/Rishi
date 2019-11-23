90fd98b05b1a44b730cd89050610bbb6c45b8699
Rishingn <51052837+Rishingn@users.noreply.github.com>
2019-11-24 01:20:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,10 +11,24 @@ function start(){
     document.querySelector(".col_1").className="col_1_open";
     document.querySelector(".col_3").className="col_3_open";
     document.querySelector(".col_2").className="col_2_open";
+    setTimeout(
+        function midDis(){
+            document.querySelector(".display").className="display-delay";
+        },600
+    );
+    
+    
 }
 
 function closeBook(){
     document.querySelector(".col_1_open").className="col_1";
     document.querySelector(".col_2_open").className="col_2";
     document.querySelector(".col_3_open").className="col_3";
+    setTimeout(
+        function midDis(){
+            document.querySelector(".display-delay").className="display";
+        },1000
+    );
 }
+
+
