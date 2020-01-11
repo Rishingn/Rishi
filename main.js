8bab71d00cf1af22ac31d1e1fa89afed1bade152
Rishingn <51052837+Rishingn@users.noreply.github.com>
2020-01-11 17:27:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function initEvents(){
     openBox.addEventListener("click", start);
     closeBox = document.querySelector(".close");
     closeBox.addEventListener("click", closeBook);
+
     $(document)
   .mousemove(function(e) {
     $('.cursor')
